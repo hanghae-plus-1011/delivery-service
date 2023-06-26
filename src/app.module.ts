74a9de3fbf9d526195fdc9cc64e96b9f671ab64e
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-06-26 11:55:26 +0900

    feat: Remove disable files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CustomerModule } from './customer/customer.module';
-import { CustomerController } from './customer/customer.controller';
-import { CustomerService } from './customer/customer.service';
-import { CartModule } from './cart/cart.module';
-import { CartController } from './cart/cart.controller';
-import { CartService } from './cart/cart.service';
-import { BookmarkModule } from './bookmark/bookmark.module';
-import { BookmarkController } from './bookmark/bookmark.controller';
-import { BookmarkService } from './bookmark/bookmark.service';
-import { NotificationModule } from './notification/notification.module';
-import { NotificationController } from './notification/notification.controller';
-import { NotificationService } from './notification/notification.service';
-import { OwnerModule } from './owner/owner.module';
-import { OwnerController } from './owner/owner.controller';
-import { OwnerService } from './owner/owner.service';
-import { StoreModule } from './store/store.module';
-import { StoreService } from './store/store.service';
-import { StoreController } from './store/store.controller';
-import { MenuModule } from './menu/menu.module';
-import { MenuController } from './menu/menu.controller';
-import { MenuService } from './menu/menu.service';
-import { ReviewModule } from './review/review.module';
-import { ReviewController } from './review/review.controller';
-import { ReviewService } from './review/review.service';
-import { OrderModule } from './order/order.module';
-import { OrderController } from './order/order.controller';
-import { OrderService } from './order/order.service';
-import { DeliveryModule } from './delivery/delivery.module';
-import { DeliveryController } from './delivery/delivery.controller';
-import { DeliveryService } from './delivery/delivery.service';
-import { PaymentModule } from './payment/payment.module';
-import { PaymentController } from './payment/payment.controller';
-import { PaymentService } from './payment/payment.service';
+import { CustomerModule } from './services/customer/customer.module';
+import { CartModule } from './services/cart/cart.module';
+import { BookmarkModule } from './services/bookmark/bookmark.module';
+import { NotificationModule } from './services/notification/notification.module';
+import { OwnerModule } from './services/owner/owner.module';
+import { StoreModule } from './services/store/store.module';
+import { MenuModule } from './services/menu/menu.module';
+import { ReviewModule } from './services/review/review.module';
+import { OrderModule } from './services/order/order.module';
+import { DeliveryModule } from './services/delivery/delivery.module';
+import { PaymentModule } from './services/payment/payment.module';
 
 @Module({
   imports: [
@@ -49,33 +26,7 @@ import { PaymentService } from './payment/payment.service';
     NotificationModule,
     OwnerModule,
   ],
-  controllers: [
-    AppController,
-    CustomerController,
-    CartController,
-    BookmarkController,
-    NotificationController,
-    StoreController,
-    OwnerController,
-    ReviewController,
-    MenuController,
-    DeliveryController,
-    PaymentController,
-    OrderController,
-  ],
-  providers: [
-    AppService,
-    StoreService,
-    CartService,
-    OwnerService,
-    MenuService,
-    NotificationService,
-    OrderService,
-    DeliveryService,
-    PaymentService,
-    BookmarkService,
-    ReviewService,
-    CustomerService,
-  ],
+  controllers: [AppController],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
