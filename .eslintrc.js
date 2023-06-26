ff603c09d9091254bbbcdf13e9def3deb817459b
Cobb Lee <49636918+archepro84@users.noreply.github.com>
2023-06-26 12:04:03 +0900

    chore: Add initialize swagger with change eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initSwagger(app);
+
   await app.listen(3000);
 }
+
+function initSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Delivery Service')
+    .setDescription('Delivery Service API')
+    .setVersion(process.env.packageVersion)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
 bootstrap();
