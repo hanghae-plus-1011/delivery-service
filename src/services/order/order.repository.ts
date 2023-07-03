f5b321da822118f23227d4022c7f6fe8a816449d
JungChihoon <jch9537@gmail.com>
2023-07-03 17:11:40 +0900

    [Feat] automock 적용 및 orderRepository 추가  On branch features/order/add-automock-repository  Changes to be committed: 	modified:   src/services/order/order.entity.ts 	modified:   src/services/order/order.module.ts 	modified:   src/services/order/order.repository.ts 	modified:   src/services/order/order.service.spec.ts 	modified:   src/services/order/order.service.ts

--- src/services/order/order.repository.ts
+++ src/services/order/order.repository.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './order.entity';
 @Injectable()
 export class OrderRepository {
+  constructor(
+    @InjectRepository(Order) private readonly orderEntity: Repository<Order>,
+  ) { }
+
   createOrder = (orderData) => {
     return true;
   };
--- src/services/order/order.service.spec.ts
+++ src/services/order/order.service.spec.ts
@@ -1,6 +1,6 @@
-// import { Test, TestingModule } from '@nestjs/testing';
 import { TestBed } from '@automock/jest';
 import { OrderService } from './order.service';
+import { OrderRepository } from './order.repository';
 
 type OrderItemsRequestInfo = {
   id: number;
@@ -15,17 +15,12 @@ type OrderRequestInfo = {
 
 describe('OrderService', () => {
   let orderService: OrderService;
-  let mockCreateOrder: jest.SpyInstance;
+  let orderRepository: jest.Mocked<OrderRepository>;
 
   beforeAll(async () => {
-    // const module: TestingModule = await Test.createTestingModule({
-    //   providers: [OrderService],
-    // }).compile();
-
-    // orderService = module.get<OrderService>(OrderService);
     const { unit, unitRef } = TestBed.create(OrderService)
-      // .mock(HttpService)
-      // .using({ get: jest.fn() })
+      .mock(OrderRepository)
+      .using({ createOrder: jest.fn() })
       // .mock(Logger)
       // .using({ log: jest.fn() })
       // .mock(CatsDal)
@@ -33,74 +28,90 @@ describe('OrderService', () => {
       .compile();
 
     orderService = unit;
-    mockCreateOrder = jest.spyOn(orderService, 'createOrder');
+    orderRepository = unitRef.get(OrderRepository);
   });
   it('should be defined', () => {
     expect(orderService).toBeDefined();
   });
 
   describe('주문 생성', () => {
+    beforeAll(async () => {
+      jest.spyOn(orderService, 'createOrder');
+    });
+
+    describe('Mocking 테스트', () => {
+      it('createOrder mocking  확인', () => {
+        expect(jest.isMockFunction(orderService.createOrder)).toBe(true);
+        expect(jest.isMockFunction(orderRepository.createOrder)).toBe(true);
+      });
+      it('automock repository Mocking 확인', () => {
+        const mockCreateOrderRequestInfo: OrderRequestInfo = {
+          id: 1,
+          orderItems: [{ id: 1, quantity: 1, storeId: 2 }],
+        };
+        orderRepository.createOrder.mockReturnValue(true);
+        orderService.createOrder(mockCreateOrderRequestInfo);
+
+        expect(orderRepository.createOrder).toBeDefined();
+        expect(orderRepository.createOrder).toHaveBeenCalled();
+        expect(orderRepository.createOrder(mockCreateOrderRequestInfo)).toEqual(
+          true,
+        );
+      });
+    });
+
     describe('주문 생성 기본 동작', () => {
       const mockCreateOrderRequestInfo: OrderRequestInfo = {
         id: 1,
         orderItems: [{ id: 1, quantity: 1, storeId: 2 }],
       };
 
-      it('createOrder mocking  확인', () => {
-        orderService.createOrder(mockCreateOrderRequestInfo);
-
-        expect(mockCreateOrder).toBeDefined();
-        expect(mockCreateOrder).toHaveBeenCalled();
-        expect(orderService.createOrder).toBe(mockCreateOrder);
-      });
-
       it('OrderService createOrder 동작 검증', () => {
-        const result = orderService.createOrder(mockCreateOrderRequestInfo);
+        orderService.createOrder(mockCreateOrderRequestInfo);
 
         expect(orderService.createOrder).toBeDefined();
         expect(orderService.createOrder).toHaveBeenCalled();
         expect(orderService.createOrder).toHaveBeenCalledWith(
           mockCreateOrderRequestInfo,
         );
-        expect(result).toBe(true);
+        expect(orderRepository.createOrder).toHaveBeenCalled();
       });
     });
-  });
 
-  describe('주문 생성 제한 조건', () => {
-    it('주문은 하나 이상이 필요하다.', () => {
-      const orderRequestInfo: OrderRequestInfo = {
-        id: 1,
-        orderItems: [],
-      };
-      expect(() => orderService.createOrder(orderRequestInfo)).toThrow();
-    });
+    describe('주문 생성 제한 조건', () => {
+      it('주문은 하나 이상이 필요하다.', () => {
+        const orderRequestInfo: OrderRequestInfo = {
+          id: 1,
+          orderItems: [],
+        };
+        expect(() => orderService.createOrder(orderRequestInfo)).toThrow();
+      });
 
-    it('한번에 수량 10개까지만 주문이 가능하다.', () => {
-      const orderRequestInfo: OrderRequestInfo = {
-        id: 1,
-        orderItems: [
-          { id: 1, quantity: 8, storeId: 2 },
-          { id: 2, quantity: 5, storeId: 2 },
-          { id: 3, quantity: 3, storeId: 2 },
-        ],
-      };
-      expect(() => orderService.createOrder(orderRequestInfo)).toThrow();
-      // 예외 발생 추가
-    });
-    it('주문은 하나의 매장에서만 주문이 가능하다.', () => {
-      const orderRequestInfo: OrderRequestInfo = {
-        id: 1,
-        orderItems: [
-          { id: 1, quantity: 1, storeId: 2 },
-          { id: 2, quantity: 1, storeId: 2 },
-          { id: 3, quantity: 1, storeId: 2 },
-          { id: 4, quantity: 1, storeId: 3 },
-          { id: 5, quantity: 1, storeId: 2 },
-          { id: 6, quantity: 1, storeId: 2 },
-        ],
-      };
-      expect(() => orderService.createOrder(orderRequestInfo)).toThrow();
+      it('한번에 수량 10개까지만 주문이 가능하다.', () => {
+        const orderRequestInfo: OrderRequestInfo = {
+          id: 1,
+          orderItems: [
+            { id: 1, quantity: 8, storeId: 2 },
+            { id: 2, quantity: 5, storeId: 2 },
+            { id: 3, quantity: 3, storeId: 2 },
+          ],
+        };
+        expect(() => orderService.createOrder(orderRequestInfo)).toThrow();
+      });
+      it('주문은 하나의 매장에서만 주문이 가능하다.', () => {
+        const orderRequestInfo: OrderRequestInfo = {
+          id: 1,
+          orderItems: [
+            { id: 1, quantity: 1, storeId: 2 },
+            { id: 2, quantity: 1, storeId: 2 },
+            { id: 3, quantity: 1, storeId: 2 },
+            { id: 4, quantity: 1, storeId: 3 },
+            { id: 5, quantity: 1, storeId: 2 },
+            { id: 6, quantity: 1, storeId: 2 },
+          ],
+        };
+        expect(() => orderService.createOrder(orderRequestInfo)).toThrow();
+      });
     });
   });
 });
