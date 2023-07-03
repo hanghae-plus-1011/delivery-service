c9b4a6efe44b457f465ef1b8d7b8a13ff0e3af8b
Flik Jeong <jsh852@gmail.com>
2023-07-03 10:47:53 +0900

    Define basic methods for controller

--- src/services/menu/menu.controller.ts
+++ src/services/menu/menu.controller.ts
@@ -1,19 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { Menu } from './menu.entity';
+import { menuDBFixture } from './menu.fixture';
 
-interface Menu {
-  id: number;
+type MenuType = Menu;
+
+export class CreateMenuDto {
+  storeId: number;
+
   menuName: string;
+
   price: number;
 }
 
-// const createMenuDto = {
-//     menuName: '김치찌개',
-//     price: 7000,
-// }
-
 @Controller('menu')
-export class Controller {
-  addMenu(menu: Omit<Menu, 'id'>) {
-    return 'addMenu';
+export class MenuController {
+  @Get(':id')
+  getMenuById(@Param('id') id: number): MenuType {
+    return menuDBFixture.find((menu) => menu.id === id);
+  }
+
+  // Q. Store의 menus에도 추가되어야 하는데 어떻게 처리하지?
+  @Post()
+  addMenu(@Body() menu: CreateMenuDto): MenuType {
+    const newMenu = new Menu(menu) as MenuType;
+    return newMenu;
+  }
+
+  @Patch(':id')
+  updateMenu(@Param('id') id: number, @Body() menu: CreateMenuDto): MenuType {
+    const menuIndex = menuDBFixture.findIndex((menuItem) => menuItem.id === id);
+    menuDBFixture[menuIndex] = { ...menuDBFixture[menuIndex], ...menu };
+    return menuDBFixture[menuIndex];
+  }
+
+  @Delete(':id')
+  deleteMenu(@Param('id') id: number): void {
+    const menuIndex = menuDBFixture.findIndex((menu) => menu.id === id);
+    menuDBFixture.splice(menuIndex, 1);
   }
 }
