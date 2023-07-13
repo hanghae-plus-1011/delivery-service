f6b19bb4b2a06bb5a5c99560f2c35351b7d6a681
SuperGenius <48075845+jch9537@users.noreply.github.com>
2023-07-13 17:56:06 +0900

    Features/add healthcheck (#17)
    * [Feat] health check 컨트롤러 추가
     On branch features/add-healthcheck
     Changes to be committed:
    	modified:   src/app.controller.ts
    	modified:   src/app.module.ts
    	new file:   src/services/healthCheck.controller.ts
    
    * [Feat] Health Check 코드 수정 - 상태코드 추가
     On branch features/add-healthcheck
     Changes to be committed:
    	modified:   Dockerfile
    	modified:   docker-compose.local.yml
    	modified:   src/services/healthCheck.controller.ts

--- src/services/healthCheck.controller.ts
+++ src/services/healthCheck.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 
 @Controller()
 export class HealthCheckController {
   @Get('/healthcheck')
+  @HttpCode(200)
   checkHealth() {
+    console.log('health check!!');
     return 'OK';
   }
 }
