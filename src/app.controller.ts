bcd780c8a8ea6223351d4e53d027eccffc405714
SuperGenius <48075845+jch9537@users.noreply.github.com>
2023-07-13 16:34:42 +0900

    [Feat] health check 컨트롤러 추가 (#16)
    On branch features/add-healthcheck
     Changes to be committed:
    	modified:   src/app.controller.ts
    	modified:   src/app.module.ts
    	new file:   src/services/healthCheck.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  healthCheck(): string {
-    return 'Success';
-  }
+  // @Get()
+  // healthCheck(): string {
+  //   return 'Success';
+  // }
 }
