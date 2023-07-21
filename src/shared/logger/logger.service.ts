f733957cae4d2c5ff497dfe32d9bddd669fbfce5
SuperGenius <48075845+jch9537@users.noreply.github.com>
2023-07-21 16:02:56 +0900

    Features/logging/add logging cloudwatch (#27)
    * [Feat] winston-cloudwatch 연결
    
    1. loggerModule, loggerService 추가 및 적용
    2. order dto 추가
    3. task 작업 정의 수정
     On branch features/logging/add-logging-cloudwatch
     Changes to be committed:
    	modified:   .aws/task-definition.json
    	modified:   src/app.controller.ts
    	modified:   src/app.module.ts
    	new file:   src/app.service.ts
    	deleted:    src/libs/logger/logger.service.ts
    	modified:   src/main.ts
    	new file:   src/services/order-item/dto/createOrderItem.dto.ts
    	modified:   src/services/order/dto/createOrder.dto.ts
    	modified:   src/services/order/order.controller.ts
    	modified:   src/services/order/order.module.ts
    	modified:   src/services/order/order.repository.ts
    	renamed:    src/libs/database/database.module.ts -> src/shared/libs/database/database.module.ts
    	new file:   src/shared/logger/logger.module.ts
    	new file:   src/shared/logger/logger.service.ts
    	new file:   src/shared/shared.module.ts
    
    * [Modify] secretManager의 환경변수 참조위한 task 정의 수정
     On branch features/logging/add-logging-cloudwatch
     Changes to be committed:
    	modified:   .aws/task-definition.json
    
    * [Modify] task 정의 수정
     On branch features/logging/add-logging-cloudwatch
     Changes to be committed:
    	modified:   .aws/task-definition.json
    
    * [Modify] winston-cloudwatch 로그 스트림 옵션제거
     On branch features/logging/add-logging-cloudwatch
     Changes to be committed:
    	modified:   .aws/task-definition.json
    	modified:   src/shared/logger/logger.service.ts
    
    * [Modify] 환경변수 설정 수정
     On branch features/logging/add-logging-cloudwatch
     Changes to be committed:
    	modified:   .aws/task-definition.json
    
    * [Modify] winston-cloudwatch 옵션추가
     On branch features/logging/add-logging-cloudwatch
     Changes to be committed:
    	modified:   .aws/task-definition.json
    	modified:   src/shared/logger/logger.service.ts

--- src/shared/logger/logger.service.ts
+++ src/shared/logger/logger.service.ts
@@ -100,6 +100,13 @@ export class AppLogger implements LoggerService {
           `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(
             additionalInfo,
           )}}`,
+        awsOptions: {
+          credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          },
+          region: process.env.AWS_REGION,
+        },
       };
 
       this.winstonLogger = createLogger({
